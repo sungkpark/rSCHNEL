76b5b4463ddd3cbe9be494aa28fca4fa78702c2f
Sung kyung Park <skpark0605@gmail.com>
2020-07-16 12:27:10 +0200

    reticulate hsne_parser

--- R/HSNE_reticulate.R
+++ R/HSNE_reticulate.R
@@ -10,7 +10,8 @@ reticulate::source_python("./py/src_clustering_HSNE_parser.py", envir = parent.f
 #' @return A vector of labels.
 parse_and_cluster <- function(filepath, scale_num, prop_method = "cluster") {
   #hsne <- pyClus.read_HSNE_binary(filepath)
-  hsne <- reticulate::py$read_HSNE_binary(filepath)
+  hsne <- reticulate::py$HSNE_parser$read_HSNE_binary(filepath)
+  #hsne <- reticulate::py$read_HSNE_binary(filepath)
   result <- hsne$cluster_scale(scale_num, prop_method)
   return(result)
 }
@@ -21,6 +22,7 @@ parse_and_cluster <- function(filepath, scale_num, prop_method = "cluster") {
 #' @return An integer of number of scales.
 get_num_scales <- function(filepath) {
   #hsne <- pyClus.read_HSNE_binary(filepath)
-  hsne <- reticulate::py$read_HSNE_binary(filepath)
+  hsne <- reticulate::py$HSNE_parser$read_HSNE_binary(filepath)
+  #hsne <- reticulate::py$read_HSNE_binary(filepath)
   return(hsne$num_scales)
 }
