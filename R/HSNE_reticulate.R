8b8706fae1bfeded0faac424c1f79f9f500d8888
Sung kyung Park <skpark0605@gmail.com>
2020-07-16 13:17:41 +0200

    set py ret environment to rschenl

--- R/HSNE_reticulate.R
+++ R/HSNE_reticulate.R
@@ -1,6 +1,6 @@
 # import parser and clustering of python implementation to R.
 # Used over R implementation for speed in reading binary file.
-reticulate::source_python("./py/src_clustering_HSNE_parser.py", envir = parent.frame())
+reticulate::source_python("./py/src_clustering_HSNE_parser.py", envir = parent.env(parent.frame()))
 
 #' Parses and clusters a HSNE file.
 #'
@@ -10,7 +10,7 @@ reticulate::source_python("./py/src_clustering_HSNE_parser.py", envir = parent.f
 #' @return A vector of labels.
 parse_and_cluster <- function(filepath, scale_num, prop_method = "cluster") {
   #hsne <- pyClus.read_HSNE_binary(filepath)
-  hsne <- reticulate::py$HSNE_parser$read_HSNE_binary(filepath)
+  hsne <- reticulate::py$read_HSNE_binary(filepath)
   #hsne <- reticulate::py$read_HSNE_binary(filepath)
   result <- hsne$cluster_scale(scale_num, prop_method)
   return(result)
