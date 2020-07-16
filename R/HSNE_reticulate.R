aef2fbcec59d769de4a7be3a280a80c57565f16f
Sung kyung Park <skpark0605@gmail.com>
2020-07-16 09:25:44 +0200

    py to globalenv

--- R/HSNE_reticulate.R
+++ R/HSNE_reticulate.R
@@ -1,6 +1,6 @@
 # import parser and clustering of python implementation to R.
 # Used over R implementation for speed in reading binary file.
-reticulate::source_python("./py/src_clustering_HSNE_parser.py")
+reticulate::source_python("./py/src_clustering_HSNE_parser.py", envir = globalenv())
 
 #' Parses and clusters a HSNE file.
 #'
@@ -9,6 +9,7 @@ reticulate::source_python("./py/src_clustering_HSNE_parser.py")
 #' @param prop_method Leiden algorithm prop method. Either "cluster" or "label"
 #' @return A vector of labels.
 parse_and_cluster <- function(filepath, scale_num, prop_method = "cluster") {
+  #hsne <- pyClus$read_HSNE_binary(filepath)
   hsne <- read_HSNE_binary(filepath)
   result <- hsne$cluster_scale(scale_num, prop_method)
   return(result)
@@ -19,6 +20,7 @@ parse_and_cluster <- function(filepath, scale_num, prop_method = "cluster") {
 #' @param filepath Path to HSNE file.
 #' @return An integer of number of scales.
 get_num_scales <- function(filepath) {
+  #hsne <- pyClus$read_HSNE_binary(filepath)
   hsne <- read_HSNE_binary(filepath)
   return(hsne$num_scales)
 }
